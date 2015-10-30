4f9d80879f7d36f644193ebe19f3e59efae0b05c
root <root@i-7f3b5dd9.csd07cloud.internal>
2015-10-30 23:02:51 +0000

    Initalized gradle for the project on Advania machine

---
+++ src/main/java/is/ru/TicTacToe/World.java
@@ -0,0 +1,10 @@
+package is.ru.TicTacToe;
+
+public class World {
+
+    private String greeting = "Hello world!";
+
+    public String greet() {
+        return greeting;
+    }
+}
---
+++ src/test/java/is/ru/TicTacToe/WorldTest.java
@@ -0,0 +1,14 @@
+package is.ru.TicTacToe;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class WorldTest {
+
+    @Test
+    public void greetResultsInHello() {
+        World world = new World();
+        assertEquals("Hello world!", world.greet()); }
+
+}
