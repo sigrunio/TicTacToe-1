4c094ae1a37279fdff2a587c4d92ab34fcbf03fb
Gunnar Gylfason <gunnigylfa@i-7f3b5dd9.csd07cloud.internal>
2015-10-31 11:33:21 +0000

    change name of package to organization name

---
+++ src/main/java/is/task/TicTacToe/TicTacToe.java
@@ -0,0 +1,8 @@
+package is.ru.TicTacToe;
+
+public class TicTacToe {                    
+    public static void main(String[] args) {
+    World world = new World();
+    System.out.println(world.greet());
+    }
+}
