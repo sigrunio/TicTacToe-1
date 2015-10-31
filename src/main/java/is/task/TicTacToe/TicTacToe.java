e6e715b4798c037e865d38b54b8ec4bb60e6a16d
Alexander Ludvigsson <Alexander@Alexanders-MBP.fios-router.home>
2015-10-31 14:56:34 -0400

    Spark Web framework added to TicTacToe

--- src/main/java/is/task/TicTacToe/TicTacToe.java
+++ src/main/java/is/task/TicTacToe/TicTacToe.java
@@ -1,8 +1,14 @@
 package is.task.TicTacToe;
 
+import spark.*;
+import static spark.Spark.*;
+import spark.servlet.SparkApplication;
+
 public class TicTacToe {                    
     public static void main(String[] args) {
-    World world = new World();
-    System.out.println(world.greet());
+    	World world = new World();
+    	System.out.println(world.greet());
+
+	get("/", (req, res) -> world.greet());
     }
 }
