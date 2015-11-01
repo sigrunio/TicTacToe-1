0da96d900f8aa8057901d56290c91367e2a57f28
sigrunio <sigruno11@ru.is>
2015-11-01 20:35:13 +0000

    Added Selenium test for Hello world

--- src/selenium/java/is/task/TicTacToe/TicTacToeWebTest.java
+++ src/selenium/java/is/task/TicTacToe/TicTacToeWebTest.java
@@ -1,45 +1,71 @@
-package is.task.TicTacToe;
+package com.example.tests;
 
+import java.util.regex.Pattern;
 import java.util.concurrent.TimeUnit;
-import static org.junit.Assert.assertEquals;
-
-import org.junit.AfterClass;
-import org.junit.After;
-import org.junit.Before;
-import org.junit.BeforeClass;
-import org.junit.Test;
-
-import org.openqa.selenium.By;
+import org.junit.*;
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+import org.openqa.selenium.*;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.support.ui.*;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
 
+public class Test {
+  private WebDriver driver;
+  private String baseUrl;
+  private boolean acceptNextAlert = true;
+  private StringBuffer verificationErrors = new StringBuffer();
 
-public class TicTacToeWebTest {
+  @Before
+  public void setUp() throws Exception {
+    driver = new FirefoxDriver();
+    baseUrl = "https://blooming-mountain-4990.herokuapp.com/hello";
+    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+  }
 
-    static WebDriver driver;
-    static String baseUrl;
+  @Test
+  public void test() throws Exception {
+    assertEquals("Hello World", driver.findElement(By.cssSelector("body")).getText());
+  }
 
-    @BeforeClass
-    public static void before() {
-        driver = new FirefoxDriver();
-        baseUrl = "http://tasktictactoe.herokuapp.com/";
-        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+  @After
+  public void tearDown() throws Exception {
+    driver.quit();
+    String verificationErrorString = verificationErrors.toString();
+    if (!"".equals(verificationErrorString)) {
+      fail(verificationErrorString);
     }
+  }
 
-    @AfterClass
-    public static void after() {
-        driver.close();
+  private boolean isElementPresent(By by) {
+    try {
+      driver.findElement(by);
+      return true;
+    } catch (NoSuchElementException e) {
+      return false;
     }
+  }
 
-    @Before
-    public void setup() {
-        ;
+  private boolean isAlertPresent() {
+    try {
+      driver.switchTo().alert();
+      return true;
+    } catch (NoAlertPresentException e) {
+      return false;
     }
+  }
 
-    @After
-    public void tearDown() {
-        driver.get(baseUrl + "/resetName");
+  private String closeAlertAndGetItsText() {
+    try {
+      Alert alert = driver.switchTo().alert();
+      String alertText = alert.getText();
+      if (acceptNextAlert) {
+        alert.accept();
+      } else {
+        alert.dismiss();
+      }
+      return alertText;
+    } finally {
+      acceptNextAlert = true;
     }
+  }
 }
