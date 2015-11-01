b9fd0d377722da82f760eea81144897a5747e7b4
sigrunio <sigruno11@ru.is>
2015-11-01 22:14:12 +0000

    Renamed class in TicTacToeWebTest

--- src/selenium/java/is/task/TicTacToe/TicTacToeWebTest.java
+++ src/selenium/java/is/task/TicTacToe/TicTacToeWebTest.java
@@ -9,7 +9,7 @@ import org.openqa.selenium.*;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.Select;
 
-public class Test {
+public class TicTacToeWebTest {
   private WebDriver driver;
   private String baseUrl;
   private boolean acceptNextAlert = true;
