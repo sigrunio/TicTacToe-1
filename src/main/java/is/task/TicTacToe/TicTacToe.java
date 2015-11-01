4c7fa973a1cb4245a260c36b0765d9469acffb83
root <root@i-7f3b5dd9.csd07cloud.internal>
2015-11-01 18:33:30 +0000

    Removed some files

--- src/main/java/is/task/TicTacToe/TicTacToe.java
+++
@@ -1,8 +0,0 @@
-package is.task.TicTacToe;
-
-public class TicTacToe {                    
-    public static void main(String[] args) {
-    World world = new World();
-    System.out.println(world.greet());
-    }
-}
