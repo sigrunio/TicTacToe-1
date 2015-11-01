1d26e0b7d1e59cf096a39c41e133f226afaaccd1
root <root@i-7f3b5dd9.csd07cloud.internal>
2015-11-01 15:25:12 +0000

    Added spark dependencies

--- src/main/java/is/task/TicTacToe/TicTacToeWeb.java
+++ src/main/java/is/task/TicTacToe/TicTacToeWeb.java
@@ -1,8 +1,10 @@
+package is.task.TicTacToe;
+
 import spark.*;
 import static spark.Spark.*;
 import spark.servlet.SparkApplication;
 
-public class HelloWorld {
+public class TicTacToeWeb {
     public static void main(String[] args) {
         get("/hello", (req, res) -> "Hello World");
     }
