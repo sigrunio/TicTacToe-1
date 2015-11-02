6cbe50ce2d4e207e60fac74b895f647428107e72
Alexander Ludvigsson <Alexander@Alexanders-MBP.fios-router.home>
2015-11-02 00:04:19 -0500

    Game class and GameTest class added

---
+++ src/main/java/is/task/TicTacToe/Game.java
@@ -0,0 +1,7 @@
+package main.java.is.task.tictactoe;
+
+import java.lang.System; 
+
+public class Game {
+
+}
---
+++ src/test/java/is/task/TicTacToe/GameTest.java
@@ -0,0 +1,13 @@
+package test.java.is.task.tictactoe;
+
+import com.sun.source.tree.AssertTree;
+import main.java.is.task.tictactoe.Game;
+import main.java.is.task.tictactoe.Square;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+
+public class GameTest {
+
+}
