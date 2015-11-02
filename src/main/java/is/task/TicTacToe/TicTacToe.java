20da98a85aac962b78345f52b316c78dcd44d4fd
Gunnar Gylfason <gunnargylfa@gmail.com>
2015-11-02 17:03:05 +0000

    tested getter and setter for ComputerPlayer property of our class

--- src/main/java/is/task/TicTacToe/TicTacToe.java
+++ src/main/java/is/task/TicTacToe/TicTacToe.java
@@ -1,5 +1,19 @@
 package main.java.is.task.TicTacToe;
 
 public class TicTacToe{
-	
+
+	// our Game to be run
+    protected Game game;
+    // our players human and computer
+    protected HumanPlayer human;
+    protected ComputerPlayer computer;
+
+    // Getters and setters
+    public ComputerPlayer getComputer() {
+        return computer;
+    }
+
+    public void setComputer(ComputerPlayer computer) {
+        this.computer = computer;
+    }
 }
--- src/test/java/is/task/TicTacToe/TicTacToeTest.java
+++ src/test/java/is/task/TicTacToe/TicTacToeTest.java
@@ -2,10 +2,33 @@ package test.java.is.task.TicTacToe;
 
 import static org.junit.Assert.assertThat;
 import static org.hamcrest.CoreMatchers.containsString;
+import static org.junit.Assert.*;
+import main.java.is.task.TicTacToe.ComputerPlayer;
+import main.java.is.task.TicTacToe.Game;
+import main.java.is.task.TicTacToe.HumanPlayer;
+import main.java.is.task.TicTacToe.TicTacToe;
 
 import org.junit.Test;
 
 
 public class TicTacToeTest {
-	;
+	    // Test getters and setters
+    @Test
+    public void testComputerPlayerGetter(){
+        TicTacToe ttt =  new TicTacToe();
+        //ttt.initializeTicTacToe();
+        ComputerPlayer cpu =  new ComputerPlayer();
+        assertEquals(ttt.getComputer().getClass(),cpu.getClass());
+    }
+
+
+    @Test
+    public void testComputerPlayerSetter(){
+        TicTacToe ttt =  new TicTacToe();
+    //    ttt.initializeTicTacToe();
+        ComputerPlayer cpu =  new ComputerPlayer();
+        ttt.setComputer(cpu);
+        assertEquals(cpu, ttt.getComputer());
+
+    }
 }
