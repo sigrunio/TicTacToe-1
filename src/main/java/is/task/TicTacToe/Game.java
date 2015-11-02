987fb8f8c80e8112b72478ff9a757ddd2c5d4e8c
Alexander Ludvigsson <Alexander@Alexanders-MBP.fios-router.home>
2015-11-02 00:29:08 -0500

    testGameInitializeBoard Passed

--- src/main/java/is/task/TicTacToe/Game.java
+++ src/main/java/is/task/TicTacToe/Game.java
@@ -4,4 +4,26 @@ import java.lang.System;
 
 public class Game {
 
+	private static Square gameBoard[][];
+
+    public Game(){
+        initializeBoard();
+    }
+
+    public Square[][] getGameBoard() {
+        return this.gameBoard;
+    }
+
+    public void initializeBoard(){
+        int squareNumber = 1;
+        this.gameBoard = new Square[3][3];
+
+        for (int i = 0;i < 3 ;i++ ) {
+            for (int j = 0;j < 3 ;j++) {
+                this.gameBoard[i][j] = new Square(squareNumber,'-',false);
+                squareNumber++;
+            }
+        }
+    }
+
 }
--- src/test/java/is/task/TicTacToe/GameTest.java
+++ src/test/java/is/task/TicTacToe/GameTest.java
@@ -9,4 +9,39 @@ import static org.junit.Assert.*;
 
 public class GameTest {
 
+	@Test
+    public void testGameInitializeBoard() {
+        Game g = new Game();
+        Square[][] temp  = g.getGameBoard();
+
+        assertEquals(1, temp[0][0].getSquareNumber());
+        assertEquals(2, temp[0][1].getSquareNumber());
+        assertEquals(3, temp[0][2].getSquareNumber());
+        assertEquals(4, temp[1][0].getSquareNumber());
+        assertEquals(5, temp[1][1].getSquareNumber());
+        assertEquals(6, temp[1][2].getSquareNumber());
+        assertEquals(7, temp[2][0].getSquareNumber());
+        assertEquals(8, temp[2][1].getSquareNumber());
+        assertEquals(9, temp[2][2].getSquareNumber());
+
+        assertEquals('-', temp[0][0].getCurrentSquareMark());
+        assertEquals('-', temp[0][1].getCurrentSquareMark());
+        assertEquals('-', temp[0][2].getCurrentSquareMark());
+        assertEquals('-', temp[1][0].getCurrentSquareMark());
+        assertEquals('-', temp[1][1].getCurrentSquareMark());
+        assertEquals('-', temp[1][2].getCurrentSquareMark());
+        assertEquals('-', temp[2][0].getCurrentSquareMark());
+        assertEquals('-', temp[2][1].getCurrentSquareMark());
+        assertEquals('-', temp[2][2].getCurrentSquareMark());
+
+        assertFalse(temp[0][0].getMarked());
+        assertFalse(temp[0][1].getMarked());
+        assertFalse(temp[0][2].getMarked());
+        assertFalse(temp[1][0].getMarked());
+        assertFalse(temp[1][1].getMarked());
+        assertFalse(temp[1][2].getMarked());
+        assertFalse(temp[2][0].getMarked());
+        assertFalse(temp[2][1].getMarked());
+        assertFalse(temp[2][2].getMarked());
+    }
 }
