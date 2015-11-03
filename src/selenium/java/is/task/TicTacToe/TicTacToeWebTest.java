2d57e30e796ea0de2a40b12c5405d419636e2839
Sigrun Inga Olafsdottir <sigruno11@i-7f3b5dd9.csd07cloud.internal>
2015-11-03 18:02:38 +0000

    Added Selenium tests

--- src/selenium/java/is/task/TicTacToe/TicTacToeWebTest.java
+++ src/selenium/java/is/task/TicTacToe/TicTacToeWebTest.java
@@ -1,4 +1,3 @@
-
 package com.example.tests;
 
 import java.util.regex.Pattern;
@@ -19,37 +18,130 @@ public class TicTacToeWebTest {
   @Before
   public void setUp() throws Exception {
     driver = new FirefoxDriver();
-    baseUrl = "http://secure-lake-2765.herokuapp.com/";
+    baseUrl = "http://tasktictactoe.herokuapp.com/";
     driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
   }
 
   @Test
-  public void testS() throws Exception {
+  public void testCheckHead() throws Exception {
+    assertEquals("TicTacToe", driver.findElement(By.cssSelector("h1.cover-heading")).getText());
+  }
+
+  @Test
+  public void testCheckInstructions() throws Exception {
+    assertEquals("Enter a number between 1 and 9, representing a square on the board, and press Make Move. You play as X.", driver.findElement(By.cssSelector("p.lead")).getText());
+  }
+
+  @Test
+  public void testMakeMovePresent() throws Exception {
+    try {
+      assertTrue(isElementPresent(By.xpath("//button[@type='submit']")));
+    } catch (Error e) {
+      verificationErrors.append(e.toString());
+    }
+  }
+
+  @Test
+  public void testNewGamePresent() throws Exception {
+    try {
+      assertTrue(isElementPresent(By.xpath("(//button[@type='submit'])[2]")));
+    } catch (Error e) {
+      verificationErrors.append(e.toString());
+    }
+  }
+
+  @Test
+  public void testTextFieldPresent() throws Exception {
+    try {
+      assertTrue(isElementPresent(By.id("move")));
+    } catch (Error e) {
+      verificationErrors.append(e.toString());
+    }
+  }
+
+  @Test
+  public void testIllegalInputChar() throws Exception {
     driver.get(baseUrl + "/");
     driver.findElement(By.xpath("(//button[@type='submit'])[2]")).click();
-    for (int second = 0;; second++) {
-    	if (second >= 60) fail("timeout");
-    	try { if (isElementPresent(By.id("board"))) break; } catch (Exception e) {}
-    	Thread.sleep(1000);
-    }
-
     driver.findElement(By.id("move")).clear();
     driver.findElement(By.id("move")).sendKeys("a");
     driver.findElement(By.xpath("//button[@type='submit']")).click();
     for (int second = 0;; second++) {
-    	if (second >= 60) fail("timeout");
-    	try { if (isElementPresent(By.id("board"))) break; } catch (Exception e) {}
-    	Thread.sleep(1000);
+      if (second >= 60) fail("timeout");
+      try { if (isElementPresent(By.cssSelector("div.success"))) break; } catch (Exception e) {}
+      Thread.sleep(1000);
     }
+    try {
+      assertEquals("Illegal input!", driver.findElement(By.id("results")).getText());
+    } catch (Error e) {
+      verificationErrors.append(e.toString());
+    }
+  }
 
+  @Test
+  public void testIllegalInputToHigh() throws Exception {
+    driver.get(baseUrl + "/");
+    driver.findElement(By.xpath("(//button[@type='submit'])[2]")).click();
+    driver.findElement(By.id("move")).clear();
+    driver.findElement(By.id("move")).sendKeys("10");
+    driver.findElement(By.xpath("//button[@type='submit']")).click();
     for (int second = 0;; second++) {
-    	if (second >= 60) fail("timeout");
-    	try { if (driver.findElement(By.cssSelector("div.alert-success")).isDisplayed()) break; } catch (Exception e) {}
-    	Thread.sleep(1000);
+      if (second >= 60) fail("timeout");
+      try { if (isElementPresent(By.cssSelector("div.success"))) break; } catch (Exception e) {}
+      Thread.sleep(1000);
     }
 
     try {
       assertEquals("Illegal input!", driver.findElement(By.id("results")).getText());
+    } catch (Error e) {
+      verificationErrors.append(e.toString());
+    }
+  }
+
+  @Test
+  public void testIllegalInputToSmall() throws Exception {
+    driver.get(baseUrl + "/");
+    driver.findElement(By.xpath("(//button[@type='submit'])[2]")).click();
+    driver.findElement(By.id("move")).clear();
+    driver.findElement(By.id("move")).sendKeys("0");
+    driver.findElement(By.xpath("//button[@type='submit']")).click();
+    for (int second = 0;; second++) {
+      if (second >= 60) fail("timeout");
+      try { if (isElementPresent(By.cssSelector("div.success"))) break; } catch (Exception e) {}
+      Thread.sleep(1000);
+    }
+
+    try {
+      assertEquals("Illegal input!", driver.findElement(By.id("results")).getText());
+    } catch (Error e) {
+      verificationErrors.append(e.toString());
+    }
+  }
+
+  @Test
+  public void testInputToOccupiedSquare() throws Exception {
+    driver.get(baseUrl + "/");
+    driver.findElement(By.xpath("(//button[@type='submit'])[2]")).click();
+    driver.findElement(By.id("move")).clear();
+    driver.findElement(By.id("move")).sendKeys("1");
+    driver.findElement(By.xpath("//button[@type='submit']")).click();
+    for (int second = 0;; second++) {
+      if (second >= 60) fail("timeout");
+      try { if (isElementPresent(By.id("board"))) break; } catch (Exception e) {}
+      Thread.sleep(1000);
+    }
+
+    driver.findElement(By.id("move")).clear();
+    driver.findElement(By.id("move")).sendKeys("1");
+    driver.findElement(By.xpath("//button[@type='submit']")).click();
+    for (int second = 0;; second++) {
+      if (second >= 60) fail("timeout");
+      try { if (isElementPresent(By.cssSelector("div.success"))) break; } catch (Exception e) {}
+      Thread.sleep(1000);
+    }
+
+    try {
+      assertEquals("Square Occupied!", driver.findElement(By.id("results")).getText());
     } catch (Error e) {
       verificationErrors.append(e.toString());
     }
